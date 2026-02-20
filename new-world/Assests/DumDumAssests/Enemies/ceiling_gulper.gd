9b1a9930c00eb2fde4134498104923ede646ff5d
Ryan Evans <112006148+RynEEvans@users.noreply.github.com>
2026-02-20 18:40:04 -0500

    Gulper Animation and code

--- new-world/Assests/DumDumAssests/Enemies/ceiling_gulper.gd
+++ new-world/Assests/DumDumAssests/Enemies/ceiling_gulper.gd
@@ -5,6 +5,7 @@ class_name CeilingGulperEnemy
 @onready var Lray = $LeftCast
 @onready var Rray = $RightCast
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var for_lunge_hbox = $ForwardLunge
 
 const speed = 30
 var gulp_is_chase : bool
@@ -24,6 +25,7 @@ var dir : Vector2
 const gravity = 490
 var knockback_force = 200
 var is_roaming : bool = true
+var motion_locked : bool = false
 
 
 
@@ -45,27 +47,42 @@ func update_facing_direction():
 	if dir.x > 0:
 		dir = Vector2(1,0)
 		animated_sprite.flip_h = false
+		animated_sprite.offset = Vector2.ZERO
+		for_lunge_hbox.position = Vector2.ZERO
 	elif dir.x < 0:
 		dir = Vector2(-1,0)
 		animated_sprite.flip_h = true
+		animated_sprite.offset = Vector2(70.0,0)
+		for_lunge_hbox.position = Vector2(40.0,0)
 
 func move(delta):
-	if !dead:
-		if !gulp_is_chase:
-			velocity += dir * speed * delta
-		is_roaming = true
-	elif dead:
-		velocity.x = 0
+	if !motion_locked:
+		if !dead:
+			if !gulp_is_chase:
+				velocity += dir * speed * delta
+			is_roaming = true
+		elif dead:
+			velocity.x = 0
 
 func choose(array):
 	array.shuffle()
 	return array.front()
 
+func forward_lunge():
+	velocity.x = 0
+	animated_sprite.play("ForwardLunge")
+
 func _on_direction_timer_timeout() -> void:
-	$direction_timer.wait_time = choose([3.5,4.0,4.5,5.0])
-	if (gulp_is_chase == false):
-		dir = choose([Vector2.LEFT, Vector2.RIGHT])
-		velocity.x = 0
+	if !motion_locked:
+		$direction_timer.wait_time = choose([3.5,4.0,4.5,5.0])
+		if (gulp_is_chase == false):
+			dir = choose([Vector2.LEFT, Vector2.RIGHT])
+			velocity.x = 0
 
 func _on_ray_timer_timeout() -> void:
 	ray_trigger = false
+
+
+func _on_forward_lunge_body_entered(body: Node2D) -> void:
+	print("goteeeeemm")
+	forward_lunge()
--- new-world/Assests/DumDumAssests/Enemies/gulper.gd
+++ new-world/Assests/DumDumAssests/Enemies/gulper.gd
@@ -5,6 +5,7 @@ class_name GulperEnemy
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var Lray = $LeftCast
 @onready var Rray = $RightCast
+@onready var for_lunge_hbox = $ForwardLunge
 
 const speed = 30
 var gulp_is_chase : bool
@@ -24,6 +25,7 @@ var dir : Vector2
 const gravity = 490
 var knockback_force = 200
 var is_roaming : bool = true
+var motion_locked : bool = false
 
 
 
@@ -44,28 +46,44 @@ func update_facing_direction():
 	if dir.x > 0:
 		dir = Vector2(1,0)
 		animated_sprite.flip_h = true
+		animated_sprite.offset = Vector2(70.0,0)
+		for_lunge_hbox.position = Vector2(40.0,0)
 	elif dir.x < 0:
 		dir = Vector2(-1,0)
 		animated_sprite.flip_h = false
+		animated_sprite.offset = Vector2.ZERO
+		for_lunge_hbox.position = Vector2.ZERO
 
 func move(delta):
-	if !dead:
-		if !gulp_is_chase:
-			velocity += dir * speed * delta
-		is_roaming = true
-	elif dead:
-		velocity.x = 0
+	if !motion_locked:
+		if !dead:
+			if !gulp_is_chase:
+				velocity += dir * speed * delta
+			is_roaming = true
+		elif dead:
+			velocity.x = 0
 
 func choose(array):
 	array.shuffle()
 	return array.front()
 
-func _on_direction_timer_timeout() -> void:
-	$direction_timer.wait_time = choose([3.5,4.0,4.5,5.0])
-	if (gulp_is_chase == false):
-		dir = choose([Vector2.LEFT, Vector2.RIGHT])
-		velocity.x = 0
+func forward_lunge():
+	velocity.x = 0
+	motion_locked = true
+	animated_sprite.play("ForwardLunge")
 
+func _on_direction_timer_timeout() -> void:
+	if !motion_locked:
+		$direction_timer.wait_time = choose([3.5,4.0,4.5,5.0])
+		if (gulp_is_chase == false):
+			dir = choose([Vector2.LEFT, Vector2.RIGHT])
+			velocity.x = 0
+	else:
+		motion_locked = false
 
 func _on_ray_timer_timeout() -> void:
 	ray_trigger = false
+
+func _on_forward_lunge_body_entered(body: Node2D) -> void:
+	print("goteeeeemm")
+	forward_lunge()
