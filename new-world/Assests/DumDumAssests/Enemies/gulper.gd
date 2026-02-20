541bb2e73ebf42720310722b195c8eb47a139863
Ryan Evans <112006148+RynEEvans@users.noreply.github.com>
2026-02-20 07:34:23 -0500

    Some Stuff, mostly Gulper movement

---
+++ new-world/Assests/DumDumAssests/Enemies/gulper.gd
@@ -0,0 +1,71 @@
+extends CharacterBody2D
+
+class_name GulperEnemy
+
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var Lray = $LeftCast
+@onready var Rray = $RightCast
+
+const speed = 30
+var gulp_is_chase : bool
+
+var health = 80
+var health_max = 80
+var health_min = 0
+
+var dead : bool = false
+var taking_damage : bool = false
+var damage_to_deal = 20
+var is_dealing_damage : bool = false
+var on_ceiling : bool
+var ray_trigger : bool
+
+var dir : Vector2
+const gravity = 490
+var knockback_force = 200
+var is_roaming : bool = true
+
+
+
+func _process(delta):
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		velocity.x = 0
+	if  (!Lray.is_colliding() || !Rray.is_colliding()) &&  is_on_floor() && !ray_trigger:
+		velocity.x = 0
+		ray_trigger = true
+		$ray_timer.start()
+		dir = dir * -1
+	move(delta)
+	move_and_slide()
+	update_facing_direction()
+
+func update_facing_direction():
+	if dir.x > 0:
+		dir = Vector2(1,0)
+		animated_sprite.flip_h = true
+	elif dir.x < 0:
+		dir = Vector2(-1,0)
+		animated_sprite.flip_h = false
+
+func move(delta):
+	if !dead:
+		if !gulp_is_chase:
+			velocity += dir * speed * delta
+		is_roaming = true
+	elif dead:
+		velocity.x = 0
+
+func choose(array):
+	array.shuffle()
+	return array.front()
+
+func _on_direction_timer_timeout() -> void:
+	$direction_timer.wait_time = choose([3.5,4.0,4.5,5.0])
+	if (gulp_is_chase == false):
+		dir = choose([Vector2.LEFT, Vector2.RIGHT])
+		velocity.x = 0
+
+
+func _on_ray_timer_timeout() -> void:
+	ray_trigger = false
--- new-world/Character/player.gd
+++ new-world/Character/player.gd
@@ -19,6 +19,9 @@ var direction : Vector2 = Vector2.ZERO
 var was_in_air : bool = false
 var dashing : bool = false
 var dashDirection = Vector2(1,0)
+var was_on_floor = is_on_floor()
+var can_coyote_jump : bool = false
+var jump_buffered : bool = false
 
 func _ready():
 	jetpack_fuel = 2
@@ -27,7 +30,7 @@ func _ready():
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() && can_coyote_jump == false:
 		velocity += get_gravity() * delta * player_gravity
 		was_in_air = true
 	else:
@@ -39,9 +42,15 @@ func _physics_process(delta: float) -> void:
 		was_in_air = false
 	# Handle jump.
 	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
+		if is_on_floor() || can_coyote_jump:
 			#Normal Jump from Floor
 			jump()
+			if can_coyote_jump:
+				can_coyote_jump = false
+			else:
+				if !jump_buffered:
+					jump_buffered = true
+					$jump_buffer_timer.start()
 		elif jetpack_fuel != 0:
 			#Double Jump in the Air
 			double_jump()
@@ -63,19 +72,34 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, walk_speed)
 		
+
+	var 	was_on_floor = is_on_floor()
+	
 	move_and_slide()
+	
+	#Started to fall
+	if (was_on_floor && !is_on_floor() && velocity.y >= 0):
+		can_coyote_jump = true
+		$coyote_timer.start()
+	
+	#touched ground
+	if not was_on_floor && is_on_floor():
+		if jump_buffered:
+			jump_buffered = false
+			jump()
+	
 	update_animation()
 	update_facing_direction()
 
 func update_animation():
 	if not animation_locked:
 		if not is_on_floor():
-			animated_sprite.play("JumpLoop")
+			animated_sprite.play("ExploJumpLoop")
 		else:
 			if direction.x != 0:
-				animated_sprite.play("Run")
+				animated_sprite.play("ExploRun")
 			else:
-				animated_sprite.play("Idle")
+				animated_sprite.play("ExploIdle")
 
 func update_facing_direction():
 	if direction.x > 0:
@@ -88,37 +112,40 @@ func update_facing_direction():
 func jump():
 	$jump_height_timer.start()
 	velocity.y = jump_velocity
-	animated_sprite.play("JumpStart")
+	animated_sprite.play("ExploJumpStart")
 	animation_locked = true
-	print($jump_height_timer.time_left)
+	if(is_on_ceiling()):
+		print("TOUCHING CEILING")
 	
 func double_jump():
 	velocity.y = double_jump_velocity * 3
-	animated_sprite.play("JumpDouble")	
+	animated_sprite.play("ExploJumpLoop")	
 	animation_locked = true
 	has_double_jumped = true
+	if(is_on_ceiling()):
+		print("TOUCHING CEILING")
 	
 func dash():
 	if animated_sprite.flip_h == false:
 		velocity = dashDirection.normalized() * dash_speed
 		velocity.y = 0
-		animated_sprite.play("Dash")
+		animated_sprite.play("ExploDash")
 		animation_locked = true
 		movement_locked = true
 		$dash_timer.start()
 	else:
 		velocity = dashDirection.normalized() * dash_speed
 		velocity.y = 0
-		animated_sprite.play("Dash")
+		animated_sprite.play("ExploDash")
 		animation_locked = true
 		movement_locked = true
 		$dash_timer.start()
 
 func land():
-	animated_sprite.play("JumpEnd")
+	animated_sprite.play("ExploJumpEnd")
 	animation_locked = true
  
-func _set_jetfuel(value):
+func _set_jetfuel():
 	if(jetpack_fuel == 0):
 		fuelbar.play("tankEmpty")
 	elif(jetpack_fuel == 1):
@@ -128,11 +155,11 @@ func _set_jetfuel(value):
 
 func add_jetfuel():
 	jetpack_fuel = jetpack_fuel + 1
-	_set_jetfuel(jetpack_fuel)
+	_set_jetfuel()
 
 func remove_jetfuel():
 	jetpack_fuel = jetpack_fuel - 1
-	_set_jetfuel(jetpack_fuel)
+	_set_jetfuel()
 	check_jetfuel()
 	if(jetpack_fuel == 1): 
 		$fuel_timer1.start()
@@ -155,11 +182,10 @@ func check_jetfuel():
 				pass
 		if(jetpack_fuel > 2):
 			jetpack_fuel = 2
-			_set_jetfuel(jetpack_fuel)
-	
+			_set_jetfuel()
 
 func _on_animated_sprite_2d_animation_finished() -> void:
-	if(["JumpEnd","JumpStart","JumpDouble","Dash"].has(animated_sprite.animation)):
+	if(["ExploJumpEnd","ExploJumpStart","ExploJumpDouble","ExploDash"].has(animated_sprite.animation)):
 		animation_locked = false
 
 
@@ -181,11 +207,17 @@ func _on_jump_height_timer_timeout() -> void:
 	if !(Input.is_action_pressed("jump")):
 		if (velocity.y < -75):
 			velocity.y = 75
-	else:
-		print("High Jump")
 
 func killPlayer():
 	position = %RespawnPoint.position
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	killPlayer()
+
+
+func _on_coyote_timer_timeout() -> void:
+	can_coyote_jump = false
+
+
+func _on_jump_buffer_timer_timeout() -> void:
+	jump_buffered = false
